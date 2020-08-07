0c38bee0fb9e1c6a04ef4030047fac68e1bcf0cb
sunil <snlkmr90@gmail.com>
2020-08-07 11:38:24 +0530

    added js and css scaffolding to laravelblog

---
+++ database/migrations/2020_08_07_051249_create_blog_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->increments('blog_id');
+            $table->string('blog_name', 100);
+            $table->string('blog_slug', 100);
+            $table->text('blog_description');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog');
+    }
+}
